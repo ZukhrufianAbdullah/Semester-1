183a8ec3d32f80be10dd1d92658478f7ca26eb01
ZukhrufianAbdullah <zukrufianabdullah@gmail.com>
2024-11-26 10:10:20 +0700

    Pertemuan 1

---
+++ PRAKTIKUM DASAR PEMROGRAMAN/Minggu 01/Latihan26.java
@@ -0,0 +1,11 @@
+/**
+ * Latihan26
+ */
+public class Latihan26 {
+
+    public static void main(String[] args) {
+        System.out.print("zukhrufian abdullah");
+        System.out.print("\nsaya adalah Mahasiswa Prodi Teknik Informatika");
+    
+    }
+}
---
+++ PRAKTIKUM DASAR PEMROGRAMAN/Minggu 01/Tugas1Jobsheet26.java
@@ -0,0 +1,16 @@
+public class Tugas1Jobsheet26{    
+    
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Zukhrufian Abdullah");
+        System.out.println("NIM     : 244107020236");
+        System.out.println("Kelas   : 1F");
+        System.out.println("No Absen: 26");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Alamat  : Prajekan Kidul");
+        System.out.println("No HP   : 081333552840");
+        System.out.println("Hobi    : Gaming");
+    }
+}
